62827a5830e1a236c9f86aa6ba99c4ec46f57a37
NguyenQuocAnh21095 <31001427+NguyenQuocAnh21095@users.noreply.github.com>
2021-01-29 00:10:20 +0700

    Update shape.py

--- shape.py
+++ shape.py
@@ -5,27 +5,27 @@ from abc import ABC, abstractmethod
 class AbstractShape(ABC):
 
     @abstractmethod
-    def create_shape_2D(self) -> AbstractShape2D:
+    def action_shape_2D(self) -> AbstractShape2D:
         pass
 
     @abstractmethod
-    def create_shape_3D(self) -> AbstractShape3D:
+    def action_shape_3D(self) -> AbstractShape3D:
         pass
 
 class RectangleShape(AbstractShape):
 
-    def create_shape_2D(self) -> AbstractShape2D:
+    def action_shape_2D(self) -> AbstractShape2D:
         return Rectangle2D()
 
-    def create_shape_3D(self) -> AbstractShape3D:
+    def action_shape_3D(self) -> AbstractShape3D:
         return Rectangle3D()
 
 class CircleShape(AbstractShape):
 
-    def create_shape_2D(self) -> AbstractShape2D:
+    def action_shape_2D(self) -> AbstractShape2D:
         return Cirle2D()
 
-    def create_shape_3D(self) -> AbstractShape3D:
+    def action_shape_3D(self) -> AbstractShape3D:
         return Circle3D()
 
 
@@ -35,12 +35,36 @@ class AbstractShape2D(ABC):
     def add(self):
         pass
 
+    @abstractmethod
+    def delete(self):
+        pass
+
+    @abstractmethod
+    def modify(self):
+        pass
+
 class Cirle2D(AbstractShape2D):
     def add(self):
         x = int(input('Position X: '))
         y = int(input('Position Y: '))
         r = int(input('Radius R: '))
         return f'Circle(position({x}, {y}), Radius({r}))'
+
+    def delete(self,ls):
+        for i in range(len(ls)):
+            print(f'({i+1})-{ls[i]}',end='; ')
+        print('\n')
+        j = int(input('Input number you want to delete: '))
+        del ls[j-1]
+        return ls
+    
+    def modify(self,ls):
+        for i in range(len(ls)):
+            print(f'({i+1})-{ls[i]}',end='; ')
+        print('\n')
+        j = int(input('Input number you want to modify: '))
+        ls[j-1] = self.add()
+        return ls
 
 class Rectangle2D(AbstractShape2D):
     def add(self):
@@ -50,10 +74,33 @@ class Rectangle2D(AbstractShape2D):
         h = int(input('Height H: '))
         return f'Rectangle(position({x}, {y}), width({w}), height({h}))'
 
+    def delete(self,ls):
+        for i in range(len(ls)):
+            print(f'({i+1})-{ls[i]}',end='; ')
+        print('\n')
+        j = int(input('Input number you want to delete: '))
+        del ls[j-1]
+        return ls
 
+    def modify(self,ls):
+        for i in range(len(ls)):
+            print(f'({i+1})-{ls[i]}',end='; ')
+        print('\n')
+        j = int(input('Input number you want to modify: '))
+        ls[j-1] = self.add()
+        return ls
+        
 class AbstractShape3D(ABC):
     @abstractmethod
     def add(self):
+        pass
+
+    @abstractmethod
+    def delete(self):
+        pass
+
+    @abstractmethod
+    def modify(self):
         pass
 
 class Circle3D(AbstractShape3D):
@@ -64,6 +111,22 @@ class Circle3D(AbstractShape3D):
         r = int(input('Radius R: '))
         return f'Circle(position({x}, {y}, {z}), Radius({r}))'
 
+    def delete(self,ls):
+        for i in range(len(ls)):
+            print(f'({i+1})-{ls[i]}',end='; ')
+        print('\n')
+        j = int(input('Input number you want to delete: '))
+        del ls[j-1]
+        return ls
+
+    def modify(self,ls):
+        for i in range(len(ls)):
+            print(f'({i+1})-{ls[i]}',end='; ')
+        print('\n')
+        j = int(input('Input number you want to modify: '))
+        ls[j-1] = self.add()
+        return ls
+
 class Rectangle3D(AbstractShape3D):
     def add(self):
         x = int(input('Position X: '))
@@ -72,12 +135,41 @@ class Rectangle3D(AbstractShape3D):
         w = int(input('Width W: '))
         h = int(input('Height H: '))
         l = int(input('Length L: '))
-        return f'Circle(position({x}, {y}, {z}), width({w}), height({z}), length({l}))'
+        return f'Circle(position({x}, {y}, {z}), width({w}), height({h}), length({l}))'
 
-def client_code(factory) -> None:
+    def delete(self,ls):
+        for i in range(len(ls)):
+            print(f'({i+1})-{ls[i]}',end='; ')
+        print('\n')
+        j = int(input('Input number you want to delete: '))
+        del ls[j-1]
+        return ls
 
-    product_2D = factory.create_shape_2D().add()
+    def modify(self,ls):
+        for i in range(len(ls)):
+            print(f'({i+1})-{ls[i]}',end='; ')
+        print('\n')
+        j = int(input('Input number you want to modify: '))
+        ls[j-1] = self.add()
+        return ls
 
+def print_out2D(ls1,ls2):
+    print('2D shape:')
+    print('Circle shape(%d):'% len(ls1))
+    shape_list(ls1)
+    print('\n')
+    print('Rectangle shape(%d):'% len(ls2))
+    shape_list(ls2)
+    print('\n')
+
+def print_out3D(ls1,ls2):
+    print('3D shape:')
+    print('Circle shape(%d):'% len(ls1))
+    shape_list(Cir3D_list)
+    print('\n')
+    print('Rectangle shape(%d):'% len(ls2))
+    shape_list(Rec3D_list)
+    print('\n')
 
 def shape_list(shape_l):
     for i in shape_l:
@@ -92,58 +184,59 @@ if __name__ == "__main__":
 
     while True:
         direction = input('Which direction? 2D(2), 3D(3): ')
+# 2D shape
         if direction == '2':
             shape_type = input('Which shape? Circle(c), Rectangle(r): ')
+# Circle 2D
             if shape_type == 'c':
-                action = input('Action? Add(a), Remove(r), Undo(u), Redo(re): ')
+                action = input('Action? Add(a), Remove(r), Modify(m), Undo(u), Redo(re): ')
                 if action == 'a':
-                    Cir2D_list.append(CircleShape().create_shape_2D().add())
-                    print('2D shape:')
-                    print('Circle shape(%d):'% len(Cir2D_list))
-                    shape_list(Cir2D_list)
-                    print('\n')
+                    Cir2D_list.append(CircleShape().action_shape_2D().add())
+                    print_out2D(Cir2D_list,Rec2D_list)
+                elif action == 'r':
+                    Cir2D_list = CircleShape().action_shape_2D().delete(Cir2D_list)
+                    print_out2D(Cir2D_list,Rec2D_list)
+                elif action == 'm':
+                    Cir2D_list = CircleShape().action_shape_2D().modify(Cir2D_list)
+                    print_out2D(Cir2D_list,Rec2D_list)
 
-                    print('Rectangle shape(%d):'% len(Rec2D_list))
-                    shape_list(Rec2D_list)
-                    print('\n')
+# Rectangle 2D
             elif shape_type == 'r':
-                action = input('Action? Add(a), Remove(r), Undo(u), Redo(re): ')
+                action = input('Action? Add(a), Remove(r), Modify(m), Undo(u), Redo(re): ')
                 if action == 'a':
-                    Rec2D_list.append(RectangleShape().create_shape_2D().add())
-                    print('2D shape:')
-                    print('Circle shape(%d):'% len(Cir2D_list))
-                    shape_list(Cir2D_list)
-                    print('\n')
-
-                    print('Rectangle shape(%d):'% len(Rec2D_list))
-                    shape_list(Rec2D_list)
-                    print('\n')
+                    Rec2D_list.append(RectangleShape().action_shape_2D().add())
+                    print_out2D(Cir2D_list,Rec2D_list)
+                elif action == 'r':
+                    Rec2D_list = RectangleShape().action_shape_2D().delete(Rec2D_list)
+                    print_out2D(Cir2D_list,Rec2D_list)
+                elif action == 'm':
+                    Rec2D_list = RectangleShape().action_shape_2D().modify(Rec2D_list)
+                    print_out2D(Cir2D_list,Rec2D_list)
+# 3D shape
         elif direction == '3':
             shape_type = input('Which shape? Circle(c), Rectangle(r): ')
+# Circle 3D
             if shape_type == 'c':
-                action = input('Action? Add(a), Remove(r), Undo(u), Redo(re): ')
+                action = input('Action? Add(a), Remove(r), Modify(m), Undo(u), Redo(re): ')
                 if action == 'a':
-                    Cir3D_list.append(CircleShape().create_shape_3D().add())
-                    print('3D shape:')
-                    print('Circle shape(%d):'% len(Cir3D_list))
-                    shape_list(Cir3D_list)
-                    print('\n')
+                    Cir3D_list.append(CircleShape().action_shape_3D().add())
+                    print_out3D(Cir3D_list,Rec3D_list)
+                elif action == 'r':
+                    Cir3D_list = CircleShape().action_shape_3D().delete(Cir3D_list)
+                    print_out3D(Cir3D_list,Rec3D_list)
+                elif action == 'm':
+                    Cir3D_list = CircleShape().action_shape_3D().modify(Cir3D_list)
+                    print_out3D(Cir3D_list,Rec3D_list)
 
-                    print('Rectangle shape(%d):'% len(Rec3D_list))
-                    shape_list(Rec3D_list)
-                    print('\n')
+# Rectangle 3D
             elif shape_type == 'r':
-                action = input('Action? Add(a), Remove(r), Undo(u), Redo(re): ')
+                action = input('Action? Add(a), Remove(r), Modify(m), Undo(u), Redo(re): ')
                 if action == 'a':
-                    Rec3D_list.append(RectangleShape().create_shape_3D().add())
-                    print('3D shape:')
-                    print('Circle shape(%d):'% len(Cir3D_list))
-                    shape_list(Cir3D_list)
-                    print('\n')
-
-                    print('Rectangle shape(%d):'% len(Rec3D_list))
-                    shape_list(Rec3D_list)
-                    print('\n')
-
-#    client_code(CircleShape())
-#    client_code(RectangleShape())
+                    Rec3D_list.append(RectangleShape().action_shape_3D().add())
+                    print_out3D(Cir3D_list,Rec3D_list)
+                elif action == 'r':
+                    Rec3D_list = RectangleShape().action_shape_3D().delete(Rec3D_list)
+                    print_out3D(Cir3D_list,Rec3D_list)
+                elif action == 'm':
+                    Rec3D_list = RectangleShape().action_shape_3D().modify(Rec3D_list)
+                    print_out3D(Cir3D_list,Rec3D_list)
